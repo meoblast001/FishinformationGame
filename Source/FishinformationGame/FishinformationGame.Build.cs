3597b103cfdb47e0c2178ccf24e8df28fba65dfa
Braden Walters <vc@braden-walters.info>
2025-02-16 13:43:21 +0100

    Initial commit.

---
+++ Source/FishinformationGame/FishinformationGame.Build.cs
@@ -0,0 +1,23 @@
+// Fill out hyour copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class FishinformationGame : ModuleRules
+{
+    public FishinformationGame(ReadOnlyTargetRules Target) : base(Target)   
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
+}
---
+++ Source/FishinformationGame/FishinformationGame.cpp
@@ -0,0 +1,4 @@
+#include "FishinformationGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FishinformationGame, "FishinformationGame" );
